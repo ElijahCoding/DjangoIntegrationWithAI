15eae9b7abbdf84c24324bc5bf64b0c4e7cf3a79
ElijahCoding <cuiyeqing960904@gmail.com>
2020-11-23 23:39:08 +0300

    rebuilding frontend

--- draw-app/src/App.js
+++ draw-app/src/App.js
@@ -1,23 +1,11 @@
-import React from 'react';
 import './App.css';
-import Classifier from './components/Classifier/Classifier';
-import ImageLIst from './components/ImageList/ImageList';
-import Navigation from './components/Navigation/Navigation';
-import {Route, BrowserRouter, Switch} from 'react-router-dom'
 
 function App() {
-    return (
-        <BrowserRouter>
-            <Navigation />
-            <div className='App'>
-                <Switch>
-                    <Route exact path='/' component={Classifier} />
-                    <Route exact path='/list' component={ImageLIst} />
-                    <Route exact path='*' component={Classifier} />
-                </Switch>
-            </div>
-        </BrowserRouter>
-    );
+  return (
+    <div className="App">
+      <h1>hello</h1>
+    </div>
+  );
 }
 
 export default App;
