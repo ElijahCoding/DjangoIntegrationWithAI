29ce127f197c084012677dc6c7dcb198c5295b7e
ElijahCoding <cuiyeqing960904@gmail.com>
2020-11-23 21:49:30 +0300

    structure built

--- draw-app/src/App.js
+++ draw-app/src/App.js
@@ -1,12 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Classifier from './components/Classifier/Classifier';
+import ImageLIst from './components/ImageList/ImageList';
+import Navigation from './components/Navigation/Navigation';
+import {Route, BrowserRouter, Switch} from 'react-router-dom'
 
 function App() {
-  return (
-    <div className="App">
-      <h1>hello</h1>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Navigation />
+            <div className='App'>
+                <Switch>
+                    <Route exact path='/' component={Classifier} />
+                    <Route exact path='/list' component={ImageLIst} />
+                    <Route exact path='*' component={Classifier} />
+                </Switch>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
