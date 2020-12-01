f19a1b8aa451a16cfcf2463be55f2358b3f63be9
ElijahCoding <cuiyeqing960904@gmail.com>
2020-12-01 23:57:41 +0300

    building model

--- classifier/cnn_model/main.py
+++ classifier/cnn_model/main.py
@@ -3,6 +3,10 @@ import keras
 import matplotlib.pyplot as plt
 from keras.datasets import mnist
 from keras.utils.np_utils import to_categorical
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.optimizers import Adam
+from keras.layers.convolutional import MaxPooling2D, Conv2D
 
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
@@ -11,5 +15,7 @@ X_test = np.expand_dims(X_test, axis=3)
 
 
 y_train = to_categorical(y_train, 10)
-y_test = to_categorical(y_train, 10)
+y_test = to_categorical(y_test, 10)
+
+X_train, X_test = X_train/255, X_test/255
 
