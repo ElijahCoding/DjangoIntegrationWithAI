43154d34b72a2020e4ab8df74984038298b24120
ElijahCoding <cuiyeqing960904@gmail.com>
2020-12-03 23:02:49 +0300

    image resized

--- classifier/digits/models.py
+++ classifier/digits/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from PIL import Image
 from keras.preprocessing.image import img_to_array
 from keras.preprocessing import image
+import cv2
+import numpy as np
+from tensorflow.python import ops
+from tensorflow.keras.models import load_model
 
 class Digit(models.Model):
     image = models.ImageField(upload_to='images')
@@ -15,4 +19,10 @@ class Digit(models.Model):
     def save(self, *args, **kwargs):
         img = Image.open(self.image)
         img_array = image.img_to_array(img)
+        dim = (28, 28)
+        new_img = cv2.cvtColor(img_array, cv2.COLOR_BGR2GRAY)
+        resized = cv2.resize(new_img, dim, interpolation=cv2.INTER_AREA)
+
+        ready = np.expand_dims(resized, axis=2)
+        ready = np.expand_dims(resized, axis=0)
         return super().save(*args, **kwargs)
